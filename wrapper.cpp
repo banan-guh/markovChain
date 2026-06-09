9fbf228b727279f76fae92f26611381c74e9262e
banan <leon.chen.lop@gmail.com>
2026-06-09 16:58:20 -0700

    updated wrapper

--- wrapper.cpp
+++ wrapper.cpp
@@ -13,7 +13,7 @@ PYBIND11_MODULE(markov_lib, m) {
     .def("train_from_file", &Markov::train_from_file)
     .def("generate", &Markov::generate)
     .def("generate_seeded", &Markov::generate_seeded)
-    .def("save", &Markov::save_brain)
-    .def("load", &Markov::load_brain)
+    .def("save_brain", &Markov::save_brain)
+    .def("load_brain", &Markov::load_brain)
     .def("sanitize", &Markov::sanitize);
 }
